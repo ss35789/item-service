212b0e00bcf2bed6adc92e75d37c8bed11caf3ed
ss35789 <ss35789@naver.com>
2024-06-13 02:29:19 +0900

    Create ItemRepository.java

---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,43 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+    private static Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    public Item save(Item item){
+        item.setId(++sequence);
+        store.put(sequence, item);
+        return item;
+    }
+
+    public Item findById(long id){
+        return store.get(id);
+    }
+
+    public List<Item> findAll(){
+        return new ArrayList<Item>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam){
+
+        Item findItem = findById(itemId);
+
+        findItem.setName(updateParam.getName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+
+
+}
