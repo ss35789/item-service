d26e5ad590aeeea42457c2ba9c38a181f806df79
ss35789 <ss35789@naver.com>
2024-06-13 17:51:23 +0900

    html 파일, css 파일

---
+++ src/main/java/hello/itemservice/Controller/ItemController.java
@@ -0,0 +1,13 @@
+package hello.itemservice.Controller;
+
+import hello.itemservice.domain.item.Item;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ItemController {
+
+
+}
