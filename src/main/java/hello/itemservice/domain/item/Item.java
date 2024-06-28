076afa58e02f246ce3be9e4088dd3a2b3afda08e
ss35789 <ss35789@naver.com>
2024-06-28 16:57:06 +0900

    entity 설정

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,18 +1,24 @@
 package hello.itemservice.domain.item;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
-
 @Data
+@Entity
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Integer price;
     private Integer quantity;
 
-    public Item(){
-
+    public Item() {
     }
+
     public Item(String name, Integer price, Integer quantity) {
         this.name = name;
         this.price = price;
